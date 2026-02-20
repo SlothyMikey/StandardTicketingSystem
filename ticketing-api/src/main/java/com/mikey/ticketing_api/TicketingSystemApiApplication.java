1fc2a33f197ca3d8f40e948f97987c75d510b15f
Mikey <kerbydalan076@gmail.com>
2026-02-20 12:28:46 +0800

    Initial Commit

---
+++ ticketing-api/src/main/java/com/mikey/ticketing_api/TicketingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.mikey.ticketing_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemApiApplication.class, args);
+	}
+
+}
